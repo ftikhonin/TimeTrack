2c4f98d020e6ead32494bc42206b63e7e87348a5
Fedor <fredr7@yandex.ru>
2020-04-19 19:35:13 +0500

    Added Sample data, some using in Program.cs

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
---
+++ WebApplication1/WebApplication1/SampleData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public static class SampleData
+    {
+        public static void Initialize(WorkContext context)
+        {
+            if (!context.Works.Any())
+            {
+                context.Works.AddRange(
+                    new Work
+                    {
+                        Url = "https://redmine.permenergosbyt.ru/issues/101180",
+                        Description = "Добавить флаг"
+                    },
+                    new Work
+                    {
+                        Url = "https://redmine.permenergosbyt.ru/issues/104796",
+                        Description = "Проверить корректность"
+                    });
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
