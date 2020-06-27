1afde008deb7b00d5568e260cf39a7f78aee5061
Fedor Tikhonin <fredr7@yandex.ru>
2020-06-28 00:49:36 +0500

    refs #3 add stopwatch, some fix, new controller
    Add app controller for stopwatch
    removed milliseconds, some fix css
    precommit projects tabs

---
+++ WebApplication1/WebApplication1/Controllers/AppController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AppController : Controller
+    {
+        WorkContext db;
+        public AppController(WorkContext context)
+        {
+            db = context;
+        }
+        // GET: App/Timer
+        public IActionResult Timer()
+        {
+            return View();
+        }
+
+        // GET: App/Projects
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        // GET: AppController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AppController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AppController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
